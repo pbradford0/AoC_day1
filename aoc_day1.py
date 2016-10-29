04556a8cc60f31ad0d7c1cd0a07ab714d196876a
pbradford0 <phil.w.bradford@gmail.com>
2016-10-28 20:14:27 -0400

    draft_2
    now with file input

--- aoc_day1.py
+++ aoc_day1.py
@@ -2,9 +2,20 @@
 
 import sys
 
+#loads the file into a string
+def load_file(filename):
+  f = open(filename, 'rU').read()
+  return f
+
 def main():
+  
+  #exit at the start if there is no specified file
+  if len(sys.argv) != 2:
+    print 'Please specify an input file'
+    sys.exit(1)
+
   #read the input to a variable
-  directions = str(sys.argv[1])
+  directions = load_file(sys.argv[1])
   #start at floor 0
   floor = 0
   #loop over input, +1 per ( and -1 per )
